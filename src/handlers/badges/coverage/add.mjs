dd330c40f62fc92cd5de4e0d1399ac8ff0c4d5ad
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-05 07:46:11 -0600

    fix parameter documenation bug; update liq-badge-lib

--- src/handlers/badges/coverage/add.mjs
+++ src/handlers/badges/coverage/add.mjs
@@ -6,8 +6,8 @@ import { setupCoverage } from './lib/setup-coverage'
 
 const help = {
   name        : 'Add test coverage badge',
-  summary     : 'Adds a test coverage badge to the target package README.md.',
-  description : ''
+  summary     : 'Adds a test coverage badge to the target package `README.md`.',
+  description : "Generates a test coverage badge based on the `qa/coverage/clover.xml` coverage report. It will attempt to insert the badge image at the top of the package `README.md` just under the title line. The title line is determined by the first line beginning with a single '#'. If there is no `README.md` file, the function will attempt to generate based on the `package.json` settings."
 }
 
 const method = 'put'
